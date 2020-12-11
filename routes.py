543ad31f9593a0cd10b36a8f603c323ae0bc01e8
Endless-Lemniscate <endless-lemniscate@yandex.ru>
2020-12-11 17:23:49 +0300

    /

--- routes.py
+++ routes.py
@@ -2,6 +2,6 @@ from views import upload_handle, download_handle, delete_handle
 
 
 def setup_routes(app):
-    app.router.add_route('POST', '/upload', upload_handle)
-    app.router.add_route('GET', '/download/{hash}', download_handle)
-    app.router.add_route('DELETE', '/delete/{hash}', delete_handle)
+    app.router.add_route('POST', '/file', upload_handle)
+    app.router.add_route('GET', '/file/{hash}', download_handle)
+    app.router.add_route('DELETE', '/file/{hash}', delete_handle)
