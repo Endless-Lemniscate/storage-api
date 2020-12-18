3471cdfd9d1594acecc4ef44099638a7c6fd3bb9
Endless-Lemniscate <endless-lemniscate@yandex.ru>
2020-12-18 12:17:43 +0300

    Some fixes according to advice of Dr.Web

--- handlers.py
+++ handlers.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from aiohttp import web
 import aiofiles
+import tempfile
 import hashlib
 import yaml
 
@@ -17,42 +18,49 @@ async def upload_handle(request):
 
     try:
         assert field.name == 'file'
-        filename = field.filename
-
-        md5 = hashlib.md5()
-
-        path = Path(storage_folder_path + filename)
-        async with aiofiles.open(path, 'ab') as f:
-            while True:
-                chunk = await field.read_chunk()
-                if not chunk:
-                    break
-                await f.write(chunk)
-                md5.update(chunk)
-
-        hash_of_file = md5.hexdigest()
-        Path(storage_folder_path + hash_of_file[:2]).mkdir(parents=True, exist_ok=True)
-        Path(storage_folder_path + filename).rename(storage_folder_path + hash_of_file[:2] + '/' + hash_of_file)
-
-        print("file {} uploaded".format(hash_of_file))
-        response = {"hash": hash_of_file}
-        return web.json_response(response, status=200, reason='File uploaded successfully')
     except AssertionError:
         response = {'code': 400,
                     'message': 'No field named file in headers'}
         return web.json_response(response, status=400, reason='No field named file in headers')
 
+    tmp_name = next(tempfile._get_candidate_names())
+    md5 = hashlib.md5()
+
+    tmp_path = Path(storage_folder_path + tmp_name)
+    async with aiofiles.open(tmp_path, 'ab') as f:
+        while True:
+            chunk = await field.read_chunk()
+            if not chunk:
+                break
+            await f.write(chunk)
+            md5.update(chunk)
+
+    hash_of_file = md5.hexdigest()
+    Path(storage_folder_path + hash_of_file[:2]).mkdir(parents=True, exist_ok=True)
+    Path(storage_folder_path + tmp_name).rename(storage_folder_path + hash_of_file[:2] + '/' + hash_of_file)
+
+    print("file {} uploaded".format(hash_of_file))
+    response = {"hash": hash_of_file}
+    return web.json_response(response, status=200, reason='File uploaded successfully')
+
 
 async def download_handle(request):
     name = request.match_info.get('hash', None)
-    path_to_file = storage_folder_path + name[:2] + "/" + name
 
+    # verify hash is hexadecimal
     try:
-        file = open(path_to_file, 'rb')
-        return web.Response(
-            headers={'Content-Disposition': 'Attachment'},
-            body=file
-        )
+        int(name, 16)
+    except ValueError:
+        response = {'code': 401, 'message': 'Not valid hash'}
+        return web.json_response(response, status=401, reason='Not valid hash')
+
+    path_to_file = storage_folder_path + name[:2] + "/" + name
+    try:
+        async with aiofiles.open(path_to_file, 'rb') as f:
+            return web.Response(
+                headers={'Content-Disposition': 'Attachment'},
+                body=await f.read()
+            )
     except FileNotFoundError:
         response = {'code': 400,
                     'message': 'No file with specified hash found'}
@@ -61,6 +69,14 @@ async def download_handle(request):
 
 async def delete_handle(request):
     name = request.match_info.get('hash', None)
+
+    # verify hash is hexadecimal
+    try:
+        int(name, 16)
+    except ValueError:
+        response = {'code': 401, 'message': 'Not valid hash'}
+        return web.json_response(response, status=401, reason='Not valid hash')
+
     path_to_file = storage_folder_path + name[:2] + "/" + name
 
     try:
